1062dc4843021024a22e1c225801523f486f0a6b
AJ Srivastava <aj.srivastava@monsanto.com>
2017-02-20 07:56:23 -0600

    remove app.props

--- src/main/java/com/ajsrivastava/GeneratorService.java
+++ src/main/java/com/ajsrivastava/GeneratorService.java
@@ -1,10 +1,14 @@
 package com.ajsrivastava;
 
 import com.ajsrivastava.delegate.GeneratorDelegate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GeneratorService {
@@ -12,5 +16,12 @@ public class GeneratorService {
     public ResponseEntity<byte[]> convertPDF(@RequestParam("url") String url) {
         GeneratorDelegate gd = new GeneratorDelegate(url);
         return gd.create();
+    }
+    
+    @RequestMapping("/save")
+    public ResponseEntity<Map<String, String>> s3Pdf(@RequestParam("url") String url) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "hello world");
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.OK);
     }
 }
